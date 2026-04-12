4950c7f06d90c321c76bf640effd753d5f57346c
Swilam20 <mahmoud.swilam.2020@gmail.com>
2026-04-12 18:05:25 +0200

    Initial project structure using feature-based architecture. Added .gitignore to exclude system files (e.g., .DS_Store).

---
+++ ecommerce-app/src/main/java/com/mentorship/ecommerce_app/common/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.mentorship.ecommerce_app.common.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+}
